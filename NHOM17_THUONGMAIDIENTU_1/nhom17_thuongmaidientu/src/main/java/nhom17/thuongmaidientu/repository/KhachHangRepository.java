194987bb0959dbe4fa483d35bab215778cb7c85b
Noforgive <maithaison1998@gmail.com>
2020-06-29 22:45:07 +0700

    28/6/2020

---
+++ NHOM17_THUONGMAIDIENTU_1/nhom17_thuongmaidientu/src/main/java/nhom17/thuongmaidientu/repository/KhachHangRepository.java
@@ -0,0 +1,48 @@
+package nhom17.thuongmaidientu.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import nhom17.thuongmaidientu.model.KhachHang;
+
+@Repository
+public interface KhachHangRepository extends JpaRepository<KhachHang, Integer> {
+	
+	@Query(value = "select * from khachhang",nativeQuery = true)
+	List<KhachHang> TimTatCaKhachHang();
+	
+	@Query(value = "select * from khachhang where email = ?1 and matkhau = ?2",nativeQuery = true)
+	KhachHang findKhachHang(String emailString,String matkhauString);
+	
+	@Query(value = "select * from khachhang where makh = ?1",nativeQuery = true)
+	KhachHang findKhachHangbyid(int id);
+	
+	@Query(value = "select makh,tenkh,sodienthoai,diachi,email,matkhau from danhgia inner join khachhang on danhgia.makh = khachhang.makh where masanpham = ?1",nativeQuery = true)
+	KhachHang findKHFromDanhGia(int masanpham);
+	
+	@Transactional
+	@Modifying
+	@Query(value = "update khachhang set sodienthoai = ?1, diachi = ?2 where makh = ?3",nativeQuery = true)
+	void UpdateTTKhachHang(String sodienthoaiString,String diachiString,int id);
+
+	@Transactional
+	@Modifying
+	@Query(value = "update khachhang set matkhau = ?1 where makh = ?2",nativeQuery = true)
+	void Thaydoimatkhau(String mkmoi,int makh);
+	
+	@Transactional
+	@Modifying
+	@Query(value = "update khachhang set matkhau = ?1 where email = ?2",nativeQuery = true)
+	void Thaydoimatkhau1(String mkmoi,String email);
+	
+	@Transactional
+	@Modifying
+	@Query(value = "delete from khachhang where makh = ?1",nativeQuery = true)
+	void XoaTaiKhoan(int makh);
+	
+}
